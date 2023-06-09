e406b8062feaa5a0775dd6d0ac97a657e8b066d4
Dmitry Maretov <maretov@gmail.com>
2023-06-09 22:02:11 +0300

    init repo

---
+++ src/function.js
@@ -0,0 +1,11 @@
+const capitalize = (text) => {
+    if (text === '') {
+        return '';
+    }
+
+    const firstChar = text[0].toUpperCase();
+    const restSubString = text.slice(1);
+    return `${firstChar}${restSubString}`;
+}
+
+export default capitalize;
